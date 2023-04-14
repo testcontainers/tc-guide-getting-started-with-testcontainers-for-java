7dc87ea02ee7a75c9b7fbb91f49ecf0c6b418ff9
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-04-14 20:24:27 +0530

    Applying prettier-java code formatting

--- src/main/java/com/testcontainers/demo/CustomerService.java
+++ src/main/java/com/testcontainers/demo/CustomerService.java
@@ -8,54 +8,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerService {
-    private final DBConnectionProvider connectionProvider;
 
-    public CustomerService(DBConnectionProvider connectionProvider) {
-        this.connectionProvider = connectionProvider;
-        createCustomersTableIfNotExists();
+  private final DBConnectionProvider connectionProvider;
+
+  public CustomerService(DBConnectionProvider connectionProvider) {
+    this.connectionProvider = connectionProvider;
+    createCustomersTableIfNotExists();
+  }
+
+  public void createCustomer(Customer customer) {
+    try (Connection conn = this.connectionProvider.getConnection()) {
+      PreparedStatement pstmt = conn.prepareStatement(
+        "insert into customers(id,name) values(?,?)"
+      );
+      pstmt.setLong(1, customer.id());
+      pstmt.setString(2, customer.name());
+      pstmt.execute();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    public void createCustomer(Customer customer) {
-        try (Connection conn = this.connectionProvider.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement("insert into customers(id,name) values(?,?)");
-            pstmt.setLong(1, customer.id());
-            pstmt.setString(2, customer.name());
-            pstmt.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+  public List<Customer> getAllCustomers() {
+    List<Customer> customers = new ArrayList<>();
+
+    try (Connection conn = this.connectionProvider.getConnection()) {
+      PreparedStatement pstmt = conn.prepareStatement("select id,name from customers");
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+        long id = rs.getLong("id");
+        String name = rs.getString("name");
+        customers.add(new Customer(id, name));
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+    return customers;
+  }
 
-    public List<Customer> getAllCustomers() {
-        List<Customer> customers = new ArrayList<>();
-
-        try (Connection conn = this.connectionProvider.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement("select id,name from customers");
-            ResultSet rs = pstmt.executeQuery();
-            while(rs.next()) {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                customers.add(new Customer(id,name));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return customers;
-    }
-
-    private void createCustomersTableIfNotExists() {
-        try (Connection conn = this.connectionProvider.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement(
-                """
+  private void createCustomersTableIfNotExists() {
+    try (Connection conn = this.connectionProvider.getConnection()) {
+      PreparedStatement pstmt = conn.prepareStatement(
+        """
                     create table if not exists customers (
                         id bigint not null,
                         name varchar not null,
                         primary key (id)
                     )
-                    """);
-            pstmt.execute();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+                    """
+      );
+      pstmt.execute();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+  }
 }
